385a78c9dee460c9a7ab0f3ca331ddf6eca86ded
ggornes <gerardogornes@gmail.com>
2019-09-27 09:54:35 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// The header creates links that can be used to navigate between routes
+
+const Header = () => (
+    <header>
+        <nav>
+            { /* ul>li*3>Link[to=$]{$} */}
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/roster">Roster</Link></li>
+                <li><Link to="/schedule">Schedule</Link></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default Header;
